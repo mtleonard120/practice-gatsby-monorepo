f2ead219de8e93a1f6f8657580cd504294917685
Michael Leonard <michael.leonard@cqlcorp.local>
2020-07-03 20:56:55 -0400

    Adding lerna, getting started breaking out into multiple sites

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import { Layout } from "../../packages/shared"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
