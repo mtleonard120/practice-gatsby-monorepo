bc9a32fa8b8ede120a9333be545c1751462dd2ad
Michael Leonard <michael.leonard@cqlcorp.local>
2020-07-05 13:39:09 -0400

    Flesh out pokemon detail page site1

--- packages/site1/src/templates/PokemonDetail.tsx
+++ packages/site1/src/templates/PokemonDetail.tsx
@@ -3,24 +3,58 @@ import React from "react"
 // packages
 import { graphql, PageProps } from "gatsby"
 
+// components
+import { Layout, SEO } from "@practice-gatsby-monorepo/shared"
+
 // types
 export interface IPokemonDetailData {
   pokemon: {
     name: string
+    height: string
     weight: string
+    sprites: {
+      front_default: string
+    }
   }
 }
 
 // primary component
 const PokemonDetail: React.FC<PageProps<IPokemonDetailData>> = ({
   data: {
-    pokemon: { name, weight },
+    pokemon: {
+      name,
+      height,
+      weight,
+      sprites: { front_default },
+    },
   },
 }) => {
+  const capName = `${name.charAt(0).toUpperCase() + name.slice(1)}`
+
   return (
-    <div>
-      I'm the detail page for {name}! This pokemon weighs {weight} pounds.
-    </div>
+    <Layout>
+      <SEO title={capName} />
+
+      <h1>{capName}</h1>
+
+      <div style={{ marginBottom: 10 }}>
+        <img
+          src={front_default}
+          style={{ border: "1px solid black", width: 200 }}
+        />
+      </div>
+
+      <dl>
+        <dt>Name</dt>
+        <dd>{capName}</dd>
+
+        <dt>Height</dt>
+        <dd>{`${height} ft`}</dd>
+
+        <dt>Weight</dt>
+        <dd>{`${weight} lbs`}</dd>
+      </dl>
+    </Layout>
   )
 }
 
@@ -30,7 +64,11 @@ export const query = graphql`
   query($name: String!) {
     pokemon(name: { eq: $name }) {
       name
+      height
       weight
+      sprites {
+        front_default
+      }
     }
   }
 `
